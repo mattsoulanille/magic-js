e35a9ccd0320c183b9a73fdb50efefbf3ca6c0ba
Matthew Soulanille <matthew@soulanille.net>
2016-12-25 15:36:37 -0800

    added some required things

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var io = require('socket.io')(http);
 var UUID = require('node-uuid');
 var _ = require("underscore");
 var Promise = require("bluebird");
-var fs = require('fs'),
-    PNG = require('pngjs').PNG;
+// var fs = require('fs'),
+//     PNG = require('pngjs').PNG;
 
 var repl = require("repl");
 
@@ -48,235 +48,33 @@ var kick = function(UUID) {
 local.context.kick = kick;
 local.context.test = test;
 
-var ship = require("./server/shipServer");
-var outfit = require("./server/outfitServer");
-var planet = require("./server/planetServer");
-var system = require("./server/systemServer.js");
-var collidable = require("./server/collidableServer.js");
-var medium_blaster = new outfit("Medium Blaster", 1);
-var sol = new system();
-local.context.sol = sol;
-var convexHullBuilder = require("./server/convexHullBuilder.js");
-var path = require('path');
-
-
-//var starbridge = new ship("Starbridge A", [medium_blaster], sol);
-
-var players = {};
 var gameTimeout;
 var gameloop = function(system) {
-    
-    // _.each(players, function(player) {
-    // 	player.time = new Date().getTime();
-    // 	player.render()
-    // });
-    system.render();
 
     gameTimeout = setTimeout(function() {gameloop(system)}, 0);
 }
 
-//notify clients of
-/*
-setInterval(function() {
-
-
-})
-*/
-
-// setInterval(function() {
-//     console.log(_.map(players, function(p) {return p.position}))
-// }, 5000)
-
-
-//app.get('/', 
-
-global.convexHulls = {};
-
-app.get('/objects/:objectType/:jsonUrl/convexHulls', function(req, res) {
-
-    var decoded = decodeURI(req.path);
-    var objPath = path.normalize(path.join(decoded, '../').slice(0,-1));
-    collidable.prototype.getConvexHulls(objPath).then(function(hulls) {
-	res.json({"hulls":hulls});
-    });
-
-});
-/*
-var getConvexHull = function(url) {
-    if ( !(convexHulls.hasOwnProperty(url)) ) {
-	convexHulls[url] = new convexHullBuilder(url).build()
-    }
-
-    return convexHulls[url];
-    
-}
-*/
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
-
 app.use(express.static(__dirname))
 
-
-sol.addObject({'name':'Earth', 'UUID':UUID(), 'type':'planet'});
-sol.build();
-
-var receives = 0;
-var transmits = 0;
-local.context.transmits = transmits;
-// debugging socket.io io
-/*
-setInterval(function() {
-    console.log("Transmits: ",transmits);
-    transmits = 0;
-    console.log("Receives: ", receives);
-    receives = 0;
-}, 1000);
-*/
-
-var paused = false;
 var playercount = 0;
-
+var players = {};
 local.context.players = players;
+
 io.on('connection', function(client){
-    receives ++;
+
     var userid = UUID();
     var owned_uuids = [userid];
-    var currentSystem = sol;
-    
-//    _.each(system.multiplayer, function(obj, key) {systemInfo[key] = obj;})
-//    console.log(sol.multiplayer);
-    //    console.log(systemInfo);
-    var medium_blaster = {
-	"name":"Medium Blaster",
-	"count":5
-    }
-    
-    var medium_blaster_turret = {
-	"name":"Medium Blaster Turret",
-	"count": 2
-	//temporary. Eventually, the server outfit object will make these
-    }
 
-    var ir_missile = {
-	"name":"IR Missile Launcher",
-	"count": 2
-    }
-    var heavy_blaster_turret = {
-	"name": "Heavy Blaster Turret",
-	"count": 2
-    }
-    var railgun_200mm = {
-	"name": "200mm Fixed Railgun",
-	"count": 4
-    }
-/*
-    var playerShipType = {
-	"UUID":userid
-    };
-*/
-    var flowerOfSpring = {
-	"name":"Flower of Spring",
-	"count": 1
-    }
-    var hailChaingun = {
-	"name":"Hail Chaingun",
-	"count":2
-    }
-    var dart = {
-	"name": "Vell-os Dart",
-	"outfits": [flowerOfSpring]
-    }
-    
-    var Starbridge = {
-	"name":"Starbridge A",
-	"outfits":[ir_missile, medium_blaster_turret, medium_blaster]
-    }
-    
-    var IDA_Frigate = {
-	"name": "IDA Frigate 1170",
-	"outfits":[heavy_blaster_turret, railgun_200mm, ir_missile]
-
-    }
-    var Firebird = {
-	"name":"Firebird_Thamgiir",
-	"outfits": [hailChaingun]
-//	"outfits": []
-    }
-    var shipTypes = {"Firebird":Firebird,
-		     "Starbridge":Starbridge,
-		     "IDA Frigate":IDA_Frigate,
-		     "Dart":dart};
-    var shipList = _.values(shipTypes);
-    var playerShipType = shipList[_.random(0,shipList.length-1)];
-//    var playerShipType = Firebird;
-    playerShipType.UUID = userid;
-
-    var sendSystem = function() {
-	//	console.log(myShip.buildInfo.outfits[0])
-	//console.log(currentSystem.getObjects())
-	var testSystem = {};
-	//testSystem[userid] = myShip.buildInfo; // for testing missing objects
-	client.emit('onconnected', {
-	    "playerShip":myShip.buildInfo,
-	    "id": userid,
-	    "system": currentSystem.getObjects(),
-	    "stats": _.omit(currentSystem.getStats(), userid),
-	    "paused": paused
-	});
-	transmits ++;
-    }
-    var myShip;
-    var buildShip = function(playerShipType) {
-	myShip = new ship(playerShipType, currentSystem);
-	return myShip.build().
-	    then(function() {
-	    _.each(myShip.outfitList, function(outf) {
-		_.each(outf.UUIDS, function(uuid) {
-		    owned_uuids.push(uuid);
-		});
-	    });
-	    });
-    //	.then(function() {console.log(myShip.weapons.all[0].UUID)})
-    }
-
-    var setShip = function(playerShipType) {
-	return buildShip(playerShipType)
-    	    .then(function() {
-		client.emit('setPlayerShip', myShip.buildInfo);
-		transmits += playercount;
-	    });
-    }
-		 
-
-    buildShip(playerShipType)
-    	.then(sendSystem);
-
-
-    //doesn't work yet
-    client.on('setShip', function(data) {
-	if (_.contains(_.keys(shipTypes), data)) {
-	    var shipInfo = shipTypes[data];
-	    setShip(shipInfo);
-	}
-    });
-//    console.log(owned_uuids);
-//    console.log(playerShipType);
-
-
-
-
-    players[userid] = {"ship":myShip,"io":client};
+    players[userid] = {"io":client};
     playercount = _.keys(players).length;
+
     console.log('a user connected. ' + playercount + " playing.");
-    client.on('updateProjectiles', function(stats) {
-	receives ++;
-    });
     
     client.on('updateStats', function(stats) {
-	receives ++;
 	var filtered_stats = {};
 //	console.log(stats)
 	_.each(stats, function(newStats, uuid) {
@@ -292,14 +90,7 @@ io.on('connection', function(client){
 
 	});
 	
-//	console.log(filtered_stats);
-	//	console.log(newStats);
-
 	client.broadcast.emit('updateStats', filtered_stats);
-	transmits += playercount - 1;
-//	client.broadcast.emit('test', "does this work?");
-	currentSystem.updateStats(filtered_stats);
-	
     });
 
     client.on("test", function(data) {
@@ -308,10 +99,6 @@ io.on('connection', function(client){
 	console.log(data);
     });
     
-
-    //    console.log(client);
-//    console.log(userid);
-
     
     client.on('pingTime', function(msg) {
 	receives ++;
@@ -321,9 +108,6 @@ io.on('connection', function(client){
     	    response.serverTime = new Date().getTime();
     	    client.emit('pongTime', response);
 	    transmits ++;
-//	    console.log(msg);
-	    
-
     	}
     });
 
@@ -341,50 +125,13 @@ io.on('connection', function(client){
 
 	client.emit('addObjects', toSend);
     });
-    client.on('land', function() {
-	receives ++;
-	client.broadcast.emit('removeObjects', owned_uuids);
-	transmits += playercount - 1;
-	currentSystem.removeObjects(owned_uuids);
-	owned_uuids = [userid];
-    });
-    client.on('depart', function() {
-	playerShipType = shipList[_.random(0,shipList.length-1)];
-	playerShipType.UUID = userid;
-	
-	setShip(playerShipType);
-	receives ++;
-	//client.broadcast.emit('addObjects', [myShip.buildInfo])
-	//var stats = {};
-	//stats[userid] = myShip.getStats();
-	//client.broadcast.emit('updateStats', stats);
-	//transmits += playercount - 1;
-    });
-    
+
     client.on('disconnect', function() {
 	receives ++;
 	client.broadcast.emit('removeObjects', owned_uuids)
-	transmits += playercount - 1;
-	currentSystem.removeObjects(owned_uuids);
 
 	delete players[userid];
 	console.log('a user disconnected. ' + _.keys(players).length + " playing.");
-    });
-    client.on('pause', function() {
-	receives ++;
-	paused = true;
-	clearTimeout(gameTimeout);
-	//	client.broadcast.emit('pause');
-	io.emit('pause', {for:'everyone'});
-	transmits += playercount;
-    });
-    client.on('resume', function() {
-	receives ++;
-	paused = false;
-	io.emit('resume', {for:'everyone'});
-	transmits += playercount;
-	sol.resume();
-//	gameTimeout = setTimeout(function() {gameloop(system)}, 0);
     });
 });
 
