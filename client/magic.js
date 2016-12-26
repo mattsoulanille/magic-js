ce12628aa0ff9a2b9e790ed9dfeb0df24ba6387f
Matthew Soulanille <matthew@soulanille.net>
2016-12-25 17:59:19 -0800

    card taps

--- client/magic.js
+++ client/magic.js
@@ -1,3 +1,30 @@
 "use strict";
 
+var stage = new PIXI.Container();
 
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight,{backgroundColor : 0xffffff}, {
+    resolution: window.devicePixelRatio || 1,
+    autoResize: true
+});
+
+document.body.appendChild(renderer.view);
+
+window.onresize = function() {
+    renderer.resize(window.innerWidth, window.innerHeight);
+}
+		
+
+
+var testCard = new card("test", ["not real"]);
+testCard.container.position.x = 200;
+testCard.container.position.y = 200;
+stage.addChild(testCard.container);
+
+function animate() {
+
+    renderer.render(stage);
+//    console.log('animating');
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
--- common/card.js
+++ common/card.js
@@ -1,17 +1,21 @@
-'use strict'
+'use strict';
 
 
 function card(name,types) {
     this.counters = [];
     this.name = name;
     this.tapped = false;
-    this.types = types
+    this.types = types;
     this.flipped = false;
+    card.prototype.buildGraphics();
 }
 
+card.prototype.scale = 0.5;
 card.prototype.addCounter = function(initialVal,name) {
     this.counters.push( new counter(initialVal,name));
 }
+
+
 card.prototype.duplicateCounter = function(name,newName){
     for (var i = 0; i < this.counters.length; i++){
 	if (this.counters[i].name == name){
@@ -26,4 +30,51 @@ card.prototype.findCounter = function(name){
 	    return this.counters[i];
 	}
     }
+}
+
+
+
+card.prototype.buildGraphics = function() {
+    this.container = new PIXI.Container();
+    this.cardSprite = new PIXI.Sprite.fromImage('images/back.jpg');
+    this.cardSprite.interactive = true;
+    this.cardSprite.anchor.x = 0.5;
+    this.cardSprite.anchor.y = 0.5;
+    this.cardSprite.scale.x = this.scale;
+    this.cardSprite.scale.y = this.scale;
+    this.container.addChild(this.cardSprite);
+
+
+    this.buildActions();
+    
+}
+
+card.prototype.tap = function() {
+
+    if (this.tapped) {
+	this.tapped = false;
+    }
+    else {
+	this.tapped = true;
+    }
+}
+
+
+
+card.prototype.buildActions = function() {
+    var tap = function() {
+	this.tap();
+	if (this.tapped) {
+	    this.container.rotation = Math.PI/2;
+	}
+	else {
+	    this.container.rotation = 0;
+	}
+    }.bind(this);
+
+    
+    this.cardSprite.on('mousedown', tap);
+    this.cardSprite.on('touchstart', tap);
+
+
 }
